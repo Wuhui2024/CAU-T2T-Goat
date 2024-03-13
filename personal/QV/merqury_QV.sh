b20d94df1f467eefd086e0a4469d5ecf21444dd2
Wuhui2024 <155352022+Wuhui2024@users.noreply.github.com>
2024-03-13 16:31:16 +0800

    Update merqury_QV.sh

--- personal/QV/merqury_QV.sh
+++ personal/QV/merqury_QV.sh
@@ -1,7 +1,7 @@
 set -ex
-export PATH=/export/personal/software/software/merfin/v1.0/bin:$PATH
-export MERQURY=/export/pipeline/BGenome/T2T/software/merqury-1.3/
-export PATH=/sugon/personal1/limengyuan/software/R-3.6.0/bin/:$PATH
-export R_LIBS_USER=/sugon/personal1/limengyuan/software/R-3.6.0/R_library
+export PATH=/software/merfin/v1.0/bin:$PATH
+export MERQURY=/software/merqury-1.3/
+export PATH=/software/R-3.6.0/bin/:$PATH
+export R_LIBS_USER=/software/R-3.6.0/R_library
 
-/export/pipeline/BGenome/T2T/software/merqury-1.3/merqury.sh  IMCG.k21.meryl  rsy.finally.fasta  rsy.finally
+/software/merqury-1.3/merqury.sh  IMCG.k21.meryl  rsy.finally.fasta  rsy.finally
